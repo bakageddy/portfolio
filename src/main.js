716ccd42b85b05de46076de9351b9fb98b8e2f37
Dinesh Kumar <gdineshda@gmail.com>
2025-07-07 00:02:35 +0530

    Footer add

--- src/main.js
+++ src/main.js
@@ -9,5 +9,6 @@ import '../styles/style.css';
 
 import '../styles/components/header.css';
 import '../styles/components/hero.css';
+import '../styles/components/footer.css';
 
 import '../styles/utils.css';
