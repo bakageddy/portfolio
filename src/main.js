92e662d47b2b1b8de853799ed43a3f6e493dd5d1
Dinesh Kumar <gdineshda@gmail.com>
2025-07-06 23:17:04 +0530

    Slay

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
+import '../styles/modern_normalize.css';
+import '../styles/satoshi.css';
+import '../styles/general-sans.css';
+import '../styles/space-grotesk.css';
+import '../styles/tanker.css';
+import '../styles/style.css';
 import '../styles/components/header.css';
+import '../styles/components/hero.css';
 import '../styles/utils.css';
